61421e1b42c81c2565fc4e6d681ad77640ab49c8
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-05-25 20:52:43 +0530

    Add project files.

---
+++ Method overloding Parameter diffrent/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_overloding_Parameter_diffrent
+{
+    class name
+    {
+      internal int show(int num)
+        {
+            return num;
+        }
+        internal int show(int num, int num1)
+        {
+            return num + num1;
+        }
+        internal int show(int num, int num1, int num2)
+        {
+            return num + num1 + num2;
+        }
+    }
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+
+            name n = new name();
+            int n1 = n.show(10,20,10);
+            Console.WriteLine(n1);
+            Console.ReadLine();
+        }
+    }
+}
